d4c1307d686748a67843e1a1d66d3f73e75cb9c6
wperez123456 <161953897+wperez123456@users.noreply.github.com>
2026-03-27 16:35:52 +0100

    Eureka

--- carro-service/src/main/java/com/carro/service/CarroServiceApplication.java
+++ carro-service/src/main/java/com/carro/service/CarroServiceApplication.java
@@ -2,8 +2,10 @@ package com.carro.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CarroServiceApplication {
 
 	public static void main(String[] args) {
--- moto-service/src/main/java/com/moto/service/MotoServiceApplication.java
+++ moto-service/src/main/java/com/moto/service/MotoServiceApplication.java
@@ -2,8 +2,10 @@ package com.moto.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MotoServiceApplication {
 
 	public static void main(String[] args) {
